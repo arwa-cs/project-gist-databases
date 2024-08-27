0b665c71c4e09418175f3d0aa6e2bc56dfa581b6
arwa <98755394+arwa-cs@users.noreply.github.com>
2024-08-27 20:59:29 +0300

    Update search.py

--- gists_database/search.py
+++ gists_database/search.py
@@ -1,4 +1,26 @@
 from .models import Gist
 
 def search_gists(db_connection, **kwargs):
-    pass
+    query = "SELECT * FROM gists"
+    params = {}
+
+    if github_id:
+        query += " WHERE github_id = :github_id"
+        params["github_id"] = github_id
+
+    if created_at:
+        if github_id:
+            query += " AND"
+        else:
+            query += " WHERE"
+        query += " datetime(created_at) = datetime(:created_at)"
+        params["created_at"] = created_at
+
+    cursor = db_connection.execute(query, params)
+    results = cursor.fetchall()
+
+    gists = []
+    for result in results:
+        gists.append(Gist(*result))
+
+    return gists
